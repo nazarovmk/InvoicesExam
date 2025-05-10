b1e25e01c68197cb474e9bb071b90c6143db685a
Muhammadnazar <muhammadnazarnazarov25@gmail.com>
2025-05-10 21:43:44 +0500

    Invoices

--- src/components/NotFoundComponent.jsx
+++ src/components/NotFoundComponent.jsx
@@ -1,14 +1,16 @@
 export default function NotFoundComponent() {
   return (
-    <div className="flex flex-col items-center gap-6">
-      <img className="mb-10 w-[241px] h-[200px]" src="./NotFound.svg" alt="" />
-      <h2 className="font-bold text-[20px] text-[#0C0E16]">
-        There is nothing here
-      </h2>
-      <p className="text-[12px] text-[#888EB0]">
-        Create an invoice by clicking the <b>New Invoice</b> button and get
-        started
-      </p>
+    <div className="flex flex-col items-center justify-center h-screen gap-6">
+      <div className="w-[217px] h-[160px] sm:w-[241px] sm:h-[200px] text-center">
+        <img className="mb-10" src="./NotFound.svg" alt="" />
+        <h2 className="font-bold text-[20px] mb-6 text-[#0C0E16]">
+          There is nothing here
+        </h2>
+        <p className="text-[12px] text-[#888EB0]">
+          Create an invoice by clicking the <b>New Invoice</b> button and get
+          started
+        </p>
+      </div>
     </div>
   );
 }

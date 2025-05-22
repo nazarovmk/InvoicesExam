f088065757df466f563329c83824d03adf993c14
Muhammadnazar <muhammadnazarnazarov25@gmail.com>
2025-05-22 11:10:04 +0500

    InvoicesExam

--- src/request/index.js
+++ src/request/index.js
@@ -1,57 +1,58 @@
-const baseURL = "https://json-api.uz/api/project/fn35/invoices"; // import.meta.env.VITE_BASE_URL
+const baseURL = "https://json-api.uz/api/project/fn35/invoices";
 
-// All
+// Hammasini olish (status bo‘yicha filtr bilan)
 export async function getInvoices(query = "") {
   const req = await fetch(baseURL + (query ? `?status=${query}` : ""));
-  if (req.status === 200) {
+  if (req.ok) {
     const result = await req.json();
     return result.data;
   } else {
-    throw new Error("Something went wrong!");
+    throw new Error("Something went wrong while fetching invoices.");
   }
 }
 
-// Get by id
+// ID orqali bitta invoice olish
 export async function getInvoice(id) {
-  const req = await fetch(baseURL + `/${id}`);
-  if (req.status === 200) {
+  const req = await fetch(`${baseURL}/${id}`);
+  if (req.ok) {
     const result = await req.json();
     return result;
   } else {
-    throw new Error("Something went wrong!");
+    throw new Error("Something went wrong while fetching invoice by ID.");
   }
 }
 
-// Delete by id
+// ID orqali o‘chirish
 export async function deleteById(id) {
-  const req = await fetch(baseURL + `/${id}`, {
+  const req = await fetch(`${baseURL}/${id}`, {
     method: "DELETE",
   });
-  if (req.status === 200) {
+  if (req.ok) {
     return "success";
   } else {
-    throw new Error("Something went wrong!");
+    throw new Error("Something went wrong while deleting invoice.");
   }
 }
 
-// Update by id
+// ID orqali yangilash
 export async function updateById(id, newData) {
-  const req = await fetch(baseURL + `/${id}`, {
+  const req = await fetch(`${baseURL}/${id}`, {
     method: "PATCH",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newData),
   });
-  if (req.status === 200) {
-    const result = req.json();
+
+  if (req.ok) {
+    const result = await req.json();
     return result;
   } else {
-    throw new Error("Something went wrong!");
+    throw new Error("Something went wrong while updating invoice.");
   }
 }
 
-// Add by id
+// Yangi invoice qo‘shish
 export async function addInvoices(data) {
   const req = await fetch(baseURL, {
     method: "POST",
@@ -60,10 +61,11 @@ export async function addInvoices(data) {
     },
     body: JSON.stringify(data),
   });
-  if (req.status === 200) {
-    const result = req.json();
+
+  if (req.ok) {
+    const result = await req.json();
     return result;
   } else {
-    throw new Error("Something went wrong!");
+    throw new Error("Something went wrong while adding invoice.");
   }
 }
